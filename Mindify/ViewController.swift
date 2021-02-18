cb658cb08a9ea9316a59db826ce42c5f9ee06a79
sangjinKIM93 <larger611@gmail.com>
2021-02-18 14:19:33 +0900

    Initial Commit

---
+++ Mindify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mindify
+//
+//  Created by 김상진 on 2021/02/18.
+//  Copyright © 2021 kipCalm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
